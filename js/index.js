aea304d857e094b2ccfbce85620120327a2ba9f7
MigueStarchaser <x43090@hotmail.com>
2025-01-13 13:02:44 -0600

    Filtros

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const api_url   = "https://pokeapi.co/api/v2/pokemon";
 const searchbox = document.querySelector("#searchbox");
 const searchbtn = document.querySelector("#btn");
 
-//Promesa
+/*//Promesa
 function request(uri,metod)
 {
     //METODOS HTTP
@@ -31,7 +31,129 @@ function request(uri,metod)
         .catch((error) => {
             console.error(error);
         });
+}*/
+
+
+const request = new Request("https://pokeapi.co/api/v2/type", {
+    method: "GET"
+});
+show_loader()
+fetch(request)
+    //Promesa peticion
+    .then((response) => {
+        if (response.status === 200) 
+        {
+            return response.json();
+        } else 
+        {
+            throw new Error("Something went wrong on API server!");
+        }
+    })
+    .then((response) => {
+        create_filters(response)
+    })
+    .catch((error) => {
+        console.error(error);
+    }).finally(()=>{
+        hide_loader();
+    });
+
+function create_filters(response)
+{
+    if(typeof response == "object" && response.results != undefined)
+    {
+       
+        const filter_container = document.querySelector("#filters");
+        //let items = response.results.length;
+        response.results.forEach(element => {
+        let tmp = element.url;
+        tmp = tmp.split("/");
+        let filter = `<div class="filter ${element.name} type_${tmp[6]}" id="${element.name}" >
+                        <a id="type_${tmp[6]}" href="${element.url}">${element.name.toUpperCase()}</a>
+                    </div>`;
+        filter_container.innerHTML +=filter
+       
+        });
+        let items = document.querySelectorAll(".filter");
+        for(let i=0;i<items.length;i++)
+        {
+            items[i].addEventListener("click",(e)=>
+            {
+                show_loader()
+                e.preventDefault();
+                let url         = e.target.href;
+                let id          = e.target.id;
+                get_pokemon_type(url,id)
+            });
+        }
+    }
+    
 }
+
+function create_card(url)
+{
+    //console.log(url)
+    const container = document.querySelector("#list")
+    var data = fetch(url).then((response)=>
+        {
+           // console.log(response)
+            return response.json();
+        }).then((response)=>
+        {
+            //console.log(response)
+            let card = `<div class="card">
+                <figure>
+                    <img id="pokemon_${response.id}" src="${response.sprites.front_default}" alt="avatar">
+                    <p>#${response.id}</p> 
+                    <p>${response.name.toUpperCase()}</p> 
+                    <a class="pokemon_details" href="${url}">Detalles</a>
+                </figure>
+            </div>`  
+        
+        container.innerHTML += card;
+        }).catch((Error)=>
+            {
+               console.log(Error)
+               console.log(response)
+            })
+    
+    
+    //cargamos la imagen
+   
+    //añadimos el template al contendor
+  
+}
+
+async function get_pokemon_type(url,id) 
+{
+    let filters = document.querySelectorAll(".filter");
+    let typeid  = "";
+    console.log(filters)
+    for(let i =0;i<filters.length;i++)
+    {   
+        filters[i].classList.remove("selected_filter");
+        if(filters[i].classList.contains(id))
+        {
+            console.log("la encontre")
+            filters[i].classList.add("selected_filter");
+            typeid = filters[i].id;
+        }
+    }
+    let data = await fetch(url);
+    data     = await data.json();
+    //console.log(data.pokemon);
+    hide_loader()
+    const container = document.querySelector("#list")
+    let count = `<div class="count">${ data.pokemon.length} POKEMON ${typeid.toUpperCase()} ENCONTRADOS</div>`;
+    container.innerHTML +=count;
+    data.pokemon.forEach((poke)=>
+    {
+        //console.log(poke)
+        create_card(poke.pokemon.url)
+    })
+    
+}
+
 
 function load_images(uri,id){
     //pedir los datos del pokemon
@@ -44,18 +166,27 @@ function load_images(uri,id){
         }
     })
     .then((response) => {
+        //console.log(response)
+        create_card(response.pokemon.url)
         //segunda promesa con la informacion convertida a JSON
         //console.log(response.sprites)
-        let image_uri   = response.sprites.front_default;
-        const img       = document.querySelector("#pokemon_"+id)
-        img.src         = image_uri;
-        //return response.sprites.front_default;
+       /* let image_uri   = response.sprites.front_default;
+        if(image_uri != undefined)
+        {
+            const img       = document.querySelector("#pokemon_"+id)
+            img.src         = image_uri;
+            //return response.sprites.front_default;
+        }*/
+        
     });
 }
+
+
 
 //Async / Await
 async function default_data()
 {
+   // show_loader();
     try
     {
         //lanzamos peticion a la API
@@ -70,42 +201,34 @@ async function default_data()
         //Cargamos como JSON y esperamos a que la promesa termine
         const data = await response.json();
         //console.log(data)
+        localStorage.setItem("listado",JSON.stringify(data));
+        let storage = localStorage.getItem("listado");
+        console.log(storage)
         create_list(data)
     }catch(error)
     {
         console.log(error)
+        //hide_loader()
     }
     
 }
 
 function create_list(data)
 {
-     //Obtener el contenedor
+   // hide_loader(); 
+    //Obtener el contenedor
      const container = document.querySelector("#list");
+     container.innerHTML = "";
      //console.log(container)
      //contar elementos en la respuesta
      let items       = Object.keys(data.results).length
-     console.log("se encontrarion "+items+" Pokemons")
+     //console.log("se encontrarion "+items+" Pokemons")
      //recorremos el objeto para crear los elementos
-     for(let i=0;i<=items;i++)
+     //hide_loader();
+     for(let i=0;i<items;i++)
      {
-         //console.log(data.results[i])
-         let pokemon = data.results[i];
-         let number  = (i+1);
-        
-         //creamos el template para cada elemento
-         let card = `<div class="card">
-                         <figure>
-                             <img id="pokemon_${number}" src="img/Pokeball-PNG-Free-Download.png" alt="avatar">
-                             <p>#${number}</p> 
-                             <p>${pokemon.name.toUpperCase()}</p> 
-                             <a href="${pokemon.url}">Detalles</a>
-                         </figure>
-                     </div>`  
-         //cargamos la imagen
-         let img   = load_images(pokemon.url,number)
-        //añadimos el template al contendor
-         container.innerHTML += card;
+        let pokemon =  data.results[i];
+        create_card(pokemon.url)        
      }
 }
 
@@ -124,14 +247,15 @@ searchbox.addEventListener("keydown",(e)=>
 
 searchbtn.addEventListener("click",(e)=>
 {
-    console.log("buuacr")
+    
+    //.log("buuacr")
     const container = document.querySelector("#list");
     let value       = searchbox.value;
     container.innerHTML = "";
     if(value!="")
     {   
-       
-        console.log("buuacr")
+       // show_loader();
+      //  console.log("buuacr")
         fetch(api_url+"/"+value).then((response) => {
             if (response.status === 200) 
             {
@@ -142,13 +266,13 @@ searchbtn.addEventListener("click",(e)=>
             }
         })
         .then((response) => {
-            console.log(response)
+           // console.log(response)
             let card = `<div class="card">
                             <figure>
                                 <img id="pokemon_${response.id}" src="${response.sprites.front_default}" alt="avatar">
                                 <p>#${response.id}</p> 
                                 <p>${response.name.toUpperCase()}</p> 
-                                <a href="${api_url+"/"+response.id}">Detalles</a>
+                                <a class="pokemon_details" href="${api_url+"/"+response.id}">Detalles</a>
                             </figure>
                         </div>`
             //añadimos el template al contendor
@@ -163,3 +287,17 @@ searchbtn.addEventListener("click",(e)=>
         default_data();
     }
 });
+
+
+function show_loader()
+{
+    const container = document.querySelector("#list");
+    var loader = `<div class="loader"><img class="imgloader" src="img/Pokeball-PNG-Free-Download.png"/></div>`;
+    container.innerHTML = loader;
+}
+
+function hide_loader()
+{
+    const container = document.querySelector("#list");
+    container.innerHTML = "";
+}
